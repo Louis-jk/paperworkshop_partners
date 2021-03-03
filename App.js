c0e034a37566e14705f1bce9822054ecc9cf53f6
Juno Kim <lippoint.surf0622@gmail.com>
2021-03-03 17:22:56 +0900

    파이어베이스 FCM TOKEN 및 메세징 기능 추가

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 import * as React from 'react';
-import {View, Text, StatusBar} from 'react-native';
+import {View, Text, StatusBar, Alert} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import SplashScreen from 'react-native-splash-screen';
+import messaging from '@react-native-firebase/messaging';
+// import {Root, Toast} from 'native-base';
 
 import DrawerNavigator from './navigation/DrawerNavigator';
+import {useDispatch, useSelector} from 'react-redux';
+import {setFcmToken} from './Modules/InfoReducer';
 
 // import MainScreen from './Screens/Main';
 // import PartnersScreen from './Screens/Partners';
@@ -12,10 +16,33 @@ import DrawerNavigator from './navigation/DrawerNavigator';
 // import {BottomTabNavigator} from './navigation/TabNavigator';
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  const [fFcmToken, setFfcmToken] = React.useState(null); // fcmtoken 현재 페이지 저장
+
   React.useEffect(() => {
     setTimeout(() => {
       SplashScreen.hide();
     }, 1000);
+
+    messaging()
+      .getToken()
+      .then((currentToken) => {
+        setFfcmToken(currentToken);
+        dispatch(setFcmToken(currentToken));
+      })
+      .catch((err) =>
+        Alert.alert('관리자에게 문의하세요', err.messaging(), [
+          {
+            text: '확인',
+          },
+        ]),
+      );
+
+    messaging().onMessage(async (remoteMessage) => {
+      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+      // console.log(remoteMessage);
+    });
   }, []);
 
   return (
