e2204b2e1ff74fb63800b7ec7d3d18cb8eeabeab
Juno Kim <lippoint.surf0622@gmail.com>
2021-03-31 17:16:50 +0900

    견적 API 인터셉터 정리

--- src/api/Estimate.js
+++ src/api/Estimate.js
@@ -2,6 +2,7 @@ import Send from '../utils/Send.js';
 import qs from 'qs';
 
 export default {
+  // 견적 리스트
   getList(type, step, company_id, cate1, ca_id, sfl, search) {
     return Send({
       method: 'post',
@@ -17,6 +18,7 @@ export default {
       }),
     });
   },
+  // 견적 정보 (간단)
   getDetail(pe_id, company_id) {
     return Send({
       method: 'post',
@@ -27,12 +29,33 @@ export default {
       }),
     });
   },
+  // 견적 세부 정보
   getMoreDetail(method, pe_id) {
     return Send({
       method: 'post',
       data: qs.stringify({
         method,
         pe_id,
+      }),
+    });
+  },
+  // 견적발송 및 수정
+  sendEstimate(frmData) {
+    return Send({
+      method: 'post',
+      data: frmData,
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    });
+  },
+  // 납품완료
+  sendDelivery(pd_id) {
+    return Send({
+      method: 'post',
+      data: qs.stringify({
+        method: 'proc_partner_estimate_status2',
+        pd_id,
       }),
     });
   },
