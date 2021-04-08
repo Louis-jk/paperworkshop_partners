8250a99f902b57c687ae872a6246acf6057d19f0
Juno Kim <lippoint.surf0622@gmail.com>
2021-04-08 18:12:21 +0900

    통계 API 인터셉터

---
+++ src/api/Statistics.js
@@ -0,0 +1,15 @@
+import Send from '../utils/Send.js';
+import qs from 'qs';
+
+export default {
+  // 통계 가져오기
+  getStatistics(company_id) {
+    return Send({
+      method: 'post',
+      data: qs.stringify({
+        method: 'proc_partner_statistics',
+        company_id,
+      }),
+    });
+  },
+};
