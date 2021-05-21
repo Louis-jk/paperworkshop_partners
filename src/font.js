b55c9129c0384e330b022a6e97451d54856f2e3f
Juno Kim <lippoint.surf0622@gmail.com>
2021-05-21 16:58:06 +0900

    iOS 폰트 설정

---
+++ src/font.js
@@ -0,0 +1,5 @@
+import { Platform } from 'react-native'
+
+export const SCDream4 = Platform.OS === 'android' ? 'SCDream4' : 'S-CoreDream-4Regular';
+export const SCDream5 = Platform.OS === 'android' ? 'SCDream5' : 'S-CoreDream-5Medium';
+export const SCDream6 = Platform.OS === 'android' ? 'SCDream6' : 'S-CoreDream-6Bold';
